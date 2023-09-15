475dc9057c6bb8c07c0a75f70e7e9e2af4a32e0f
Md. Arshad <arshadchowdhury46@gmail.com>
2023-09-15 23:22:19 +0600

    Added previous version of the app in previous version folder

---
+++ previous-version/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ScrollToTop from "./components/ScrollToTop";
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { StateProvider } from "./states/StateProvider";
+import reducer, { initialState } from "./states/reducer";
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <BrowserRouter>
+          <ScrollToTop />
+          <App />
+        </BrowserRouter>
+      </StateProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
