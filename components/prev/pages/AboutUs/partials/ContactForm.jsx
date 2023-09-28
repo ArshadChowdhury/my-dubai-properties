f77c899db28d19a760dbe36d03ea13d5c589ffd3
Md. Arshad <arshadchowdhury46@gmail.com>
2023-09-28 14:19:39 +0600

    Fixed modal popping issue in contact us about and terms and conditions

--- components/prev/pages/AboutUs/partials/ContactForm.jsx
+++ components/prev/pages/AboutUs/partials/ContactForm.jsx
@@ -3,8 +3,34 @@ import BtnHexagon from "@/components/prev/BtnHexagon";
 import SignUpForm from "../../HomePage/partials/SignUpForm";
 import { useEffect } from "react";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+
 import SignUpForm2 from "../../HomePage/partials/SignUpForm2";
+
 const ContactForm = (props) => {
+  const [subsPopUp, setSubsPopUp] = useState(false);
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEmail("");
+    setSubsPopUp(true);
+  }
+
+  function handleClose() {
+    setSubsPopUp(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleClose);
+    window.addEventListener("mousedown", handleClose);
+    return () => {
+      window.removeEventListener("scroll", handleClose);
+      window.removeEventListener("mousedown", handleClose);
+    };
+  }, []);
+
   const tocData = props.homeData.lang.termsAndConditions;
   return (
     <section className="">
@@ -22,7 +48,7 @@ const ContactForm = (props) => {
           <div className="w-full border"></div>
 
           <div className="md:hidden">
-            <SignUpForm2 tocData={tocData} />
+            <SignUpForm2 homeData={props?.homeData} tocData={tocData} />
           </div>
           <div className="hidden md:block">
             <div className="md:flex w-full pt-3">
@@ -31,6 +57,7 @@ const ContactForm = (props) => {
               </p>
               <div className="flex-1 flex flex-col md:flex-row justify-center items-center">
                 <form
+                  onSubmit={handleSubmit}
                   action=""
                   className="flex flex-col md:flex-row md:justify-between justify-center items-center gap-3"
                 >
@@ -38,6 +65,8 @@ const ContactForm = (props) => {
                     type="email"
                     name="email"
                     id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder={tocData?.newsletter?.email}
                     className="md:mr-3 border-[0.5px] border-[#BFA04B] px-5 py-2 bg-black bg-opacity-30  placeholder:font-montserrat placeholder:text-white"
                     required
@@ -53,6 +82,51 @@ const ContactForm = (props) => {
           </div>
         </div>
       </div>
+      {
+        <AnimatePresence>
+          {subsPopUp && (
+            <div
+              className={`w-screen h-screen justify-center items-center flex overflow-x-hidden overflow-y-auto fixed top-0 left-0 md:-bottom-18 md:left-0 transition-all md:inset-0 z-50 outline-none focus:outline-none rounded-t-[2.5rem] md:rounded-none`}
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.8)",
+              }}
+            >
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  x: -200,
+                }}
+                transition={{
+                  duration: 0.3,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                }}
+                exit={{
+                  opacity: 0,
+                  x: -200,
+                }}
+                viewport={{ once: true }}
+                className={`cursor-pointer fixed flex flex-col items-center justify-center mx-2 md:mx-0 md:py-4 md:px-10 rounded-lg font-montserrat text-white border p-3 z-50 vector_background_modal`}
+              >
+                <Image
+                  height={150}
+                  width={150}
+                  src="/images/global/footer-logo.png"
+                  alt=""
+                  className="my-2 pb-2"
+                />
+                <h1 className="text-xl">Form Submitted!</h1>
+                <p>
+                  We&apos;d like to show you notifictions for the latest news
+                  and updates
+                </p>
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>
+      }
     </section>
   );
 };
